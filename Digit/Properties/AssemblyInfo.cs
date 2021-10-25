111d46634ae1f4eeb9ab11deb58dc6d0a3c58c3f
Moleys <34809211+Moleys@users.noreply.github.com>
2021-10-25 19:05:47 +0700

    Thanh Trung

--- Digit/Properties/AssemblyInfo.cs
+++ Digit/Properties/AssemblyInfo.cs
@@ -1,4 +1,5 @@
 ﻿using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
@@ -9,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Digit")]
-[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -19,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("df62dde8-2957-46ad-8e1e-071a8610633b")]
+[assembly: Guid("5ca4d12a-4fdd-4fe1-8fae-c504bf7dc486")]
 
 // Version information for an assembly consists of the following four values:
 //
